1597c0d64cc5ec4d6723cd01f48131dd73e13574
phillipsaur <phillipdthai@gmail.com>
2024-05-23 12:08:15 -0700

    Added Home Component and updated routes

--- WorldMap/src/app/app.routes.ts
+++ WorldMap/src/app/app.routes.ts
@@ -1,3 +1,5 @@
 import { Routes } from '@angular/router';
+import { HomeHomeComponent } from './home-home/home-home.component';
+import { MapPageComponent } from './map-page/map-page.component';
 
 export const routes: Routes = [];
