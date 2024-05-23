f1731b79f828dde8e3b33853d9aa240f903f5ff8
phillipsaur <phillipdthai@gmail.com>
2024-05-23 12:12:51 -0700

    Updated routes and navbar

--- WorldMap/src/app/app.routes.ts
+++ WorldMap/src/app/app.routes.ts
@@ -2,4 +2,7 @@ import { Routes } from '@angular/router';
 import { HomeHomeComponent } from './home-home/home-home.component';
 import { MapPageComponent } from './map-page/map-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: 'home', component: HomeHomeComponent },
+    { path: 'map', component: MapPageComponent }
+];
