8aa2a346004fcbbf600cb1f6db621aaa0557c0a8
phillipsaur <phillipdthai@gmail.com>
2024-05-23 22:04:50 -0700

    Added onClick logic for the map and creates alert with countryID

--- WorldMap/src/app/map-page/map-page.component.ts
+++ WorldMap/src/app/map-page/map-page.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-map-page',
@@ -7,6 +7,24 @@ import { Component } from '@angular/core';
   templateUrl: './map-page.component.html',
   styleUrl: './map-page.component.css'
 })
-export class MapPageComponent {
+export class MapPageComponent implements OnInit{
+  constructor() { }
 
+  ngOnInit(): void {
+    const object = document.getElementById('world-map') as HTMLObjectElement;
+    object.onload = () => {
+      const svgFile = object.contentDocument;
+      const countries = svgFile?.querySelectorAll('path');
+
+      countries?.forEach(country => {
+        country.addEventListener('click', this.countryClick.bind(this));
+      });
+    };
+  }
+
+  countryClick(event:MouseEvent): void {
+    const countryClicked = event.target as SVGPathElement;
+    const countryID = countryClicked.id;
+    alert(`Country clicked: ${countryID}`)
+  }
 }
